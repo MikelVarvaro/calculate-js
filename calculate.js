e31021404985899f3103675c92e63598ec862c7a
Mikel Varvaro <mikelvarvaro@gmail.com>
2024-10-19 15:52:55 -0400

    calculate object of methods

---
+++ calculate.js
@@ -0,0 +1,17 @@
+const calculate = {
+ add:(x, y) => {
+    return +x + +y;
+  },
+  sub: (x, y) => {
+    return x - y;
+  },
+  mul: (x, y) => {
+    return x * y;
+  },
+  pow: (x, y) => {
+    return x ** y;
+  },
+}
+
+
+window.cal = calculate
